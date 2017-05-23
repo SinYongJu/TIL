c363d25948a2c3b3ac8f4c44a103472f629ccbdd
SinYongJu <ygp007@naver.com>
2017-05-23 19:38:42 +0900

    test

---
+++ Test07.java
@@ -0,0 +1,18 @@
+//자바 프로그래밍-entry point
+
+/*
+자바 스크립트의 주석과 같다
+
+
+*/
+
+
+  public class Test07{
+    public static void main(String[] args){
+      System.out.print("hello");
+      System.out.print("world\n");
+      System.out.println("hello,world");
+      System.out.print("hello");
+    }
+
+  }
